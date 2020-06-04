23cd76e6ece5ce440660dce095595b47ee0e157e
DESKTOP-L79U3LK\n64co <hazuki.aoi1@gmail.com>
2020-06-05 00:14:29 +0800

    基本的单个模型导出

---
+++ Assets/Scripts/OutputMesh.cs
@@ -0,0 +1,303 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO.MemoryMappedFiles;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class OutputMesh : MonoBehaviour
+{
+    int[] imgs = new int[32];
+
+    public byte[] ObjectToBytes(object obj)
+    {
+        using (MemoryStream ms = new MemoryStream())
+        {
+
+            // //以二进制格式将对象或整个连接对象图形序列化和反序列化。
+            IFormatter formatter = new BinaryFormatter();
+            //把字符串以二进制放进memStream中
+            formatter.Serialize(ms, obj);
+            //返回从其创建此流的无符号字节数组。 是会返回所有分配的字节，不管用没用到。
+            ////返回无符号字节数组 ，无符号字节数组 其实就是byte(0~255),有符号字节sbyte(-128~127)
+            return ms.GetBuffer();
+        }
+    }
+
+    private byte[] ToByte(bool data)
+    {
+        unsafe
+        {
+            return new byte[] { data ? (byte)0x01 : (byte)0x00 };
+            return new byte[] { *(byte*)&data };
+        }
+    }
+
+    private byte[] ToByte(float data)
+    {
+        unsafe
+        {
+            byte* pdata = (byte*)&data;
+            byte[] byteArray = new byte[sizeof(float)];
+            for (int i = 0; i < sizeof(float); ++i)
+                byteArray[i] = *pdata++;
+            return byteArray;
+        }
+    }
+
+    private byte[] ToByte(int data)
+    {
+        unsafe
+        {
+            byte* pdata = (byte*)&data;
+            byte[] byteArray = new byte[sizeof(float)];
+            for (int i = 0; i < sizeof(float); ++i)
+                byteArray[i] = *pdata++;
+            return byteArray;
+        }
+    }
+
+    private byte[] ToByte(int[] data)
+    {
+        unsafe
+        {
+            fixed (int* ptrf = data)
+            {
+                int itemSize = sizeof(int);
+                byte[] byteArray = new byte[itemSize * data.Length];
+                byte* bytes = (byte*)ptrf;
+                for (int i = 0; i < data.Length * itemSize; ++i)
+                {
+                    byteArray[i] = *bytes++;
+                }
+                return byteArray;
+            }
+        }
+    }
+
+    private byte[] ToByte(Vector2 data)
+    {
+        List<byte> result = new List<byte>(2 * sizeof(float));
+
+        result.AddRange(ToByte(data.x));
+        result.AddRange(ToByte(data.y));
+
+        return result.ToArray();
+    }
+
+    private byte[] ToByte(Vector3 data)
+    {
+        List<byte> result = new List<byte>(3 * sizeof(float));
+
+        result.AddRange(ToByte(data.x));
+        result.AddRange(ToByte(data.y));
+        result.AddRange(ToByte(data.z));
+
+        return result.ToArray();
+    }
+
+    private byte[] ToByte(Vector4 data)
+    {
+        List<byte> result = new List<byte>(4 * sizeof(float));
+
+        result.AddRange(ToByte(data.x));
+        result.AddRange(ToByte(data.y));
+        result.AddRange(ToByte(data.z));
+        result.AddRange(ToByte(data.w));
+
+        return result.ToArray();
+    }
+
+    private byte[] ToByte(Vector2[] data)
+    {
+        List<byte> result = new List<byte>(data.Length * 2 * sizeof(float));
+        for (int i = 0; i < data.Length; i++)
+        {
+            var bsx = ToByte(data[i].x);
+            var bsy = ToByte(data[i].y);
+            result.AddRange(bsx);
+            result.AddRange(bsy);
+        }
+        return result.ToArray();
+    }
+
+    private byte[] ToByte(Vector3[] data)
+    {
+        List<byte> result = new List<byte>(data.Length * 3 * sizeof(float));
+        for(int i = 0; i < data.Length; i++)
+        {
+            var bsx = ToByte(data[i].x);
+            var bsy = ToByte(data[i].y);
+            var bsz = ToByte(data[i].z);
+            result.AddRange(bsx);
+            result.AddRange(bsy);
+            result.AddRange(bsz);
+        }
+        return result.ToArray();
+    }
+
+    private byte[] ToByte(Color[] data)
+    {
+        List<byte> result = new List<byte>(data.Length * 4 * sizeof(float));
+        for (int i = 0; i < data.Length; i++)
+        {
+            var bsx = ToByte(data[i].r);
+            var bsy = ToByte(data[i].g);
+            var bsz = ToByte(data[i].b);
+            var bsw = ToByte(data[i].a);
+            result.AddRange(bsx);
+            result.AddRange(bsy);
+            result.AddRange(bsz);
+            result.AddRange(bsw);
+        }
+        return result.ToArray();
+    }
+
+    private byte[] ToByte(Vector4[] data)
+    {
+        List<byte> result = new List<byte>(data.Length * 4 * sizeof(float));
+        for (int i = 0; i < data.Length; i++)
+        {
+            var bsx = ToByte(data[i].x);
+            var bsy = ToByte(data[i].y);
+            var bsz = ToByte(data[i].z);
+            var bsw = ToByte(data[i].w);
+            result.AddRange(bsx);
+            result.AddRange(bsy);
+            result.AddRange(bsz);
+            result.AddRange(bsw);
+        }
+        return result.ToArray();
+    }
+
+    public Mesh mesh;
+    public string path = "";
+
+
+    [ContextMenu("ExportMesh")]
+    public void ExportMesh()
+    {
+        using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+        {
+            //写入顶点总数
+            var vCount = ToByte(mesh.vertexCount);
+            Debug.Log(vCount.Length);
+            fs.Write(vCount, 0, vCount.Length);
+
+            bool hadColor = mesh.colors.Length == mesh.vertexCount;
+            bool hadUV1 = mesh.uv2.Length == mesh.vertexCount;
+
+            //写入顶点数据
+            for (int i = 0; i < mesh.vertexCount; i++)
+            {
+                var ve = ToByte(mesh.vertices[i]);
+                var co = hadColor ? ToByte(mesh.colors[i]) : ToByte(Color.white);
+                var nr = ToByte(mesh.normals[i]);
+                var ta = ToByte(mesh.tangents[i]);
+                var uv0 = ToByte(mesh.uv[i]);
+                var uv1 = hadUV1 ? ToByte(mesh.uv2[i]) : ToByte(Vector2.zero);
+                fs.Write(ve, 0, ve.Length);
+                fs.Write(co, 0, co.Length);
+                fs.Write(nr, 0, nr.Length);
+                fs.Write(ta, 0, ta.Length);
+                fs.Write(uv0, 0, uv0.Length);
+                fs.Write(uv1, 0, uv1.Length);
+            }
+
+            //写入索引数据
+            var trisCount = ToByte(mesh.triangles.Length);
+            var tris = ToByte(mesh.triangles);
+
+            fs.Write(trisCount, 0, trisCount.Length);
+            fs.Write(tris, 0, tris.Length);
+
+            /*
+            var vex = ToByte(mesh.vertices);
+            var nomrals = ToByte(mesh.normals);
+            var tangents = ToByte(mesh.tangents);
+            var uv = ToByte(mesh.uv);
+            fs.Write(vex, 0, vex.Length);
+            fs.Write(nomrals, 0, nomrals.Length);
+            fs.Write(tangents, 0, tangents.Length);
+
+            //判断如果存在顶点色，则写入
+            bool hadColor = mesh.uv2.Length == mesh.vertexCount;
+            fs.Write(ToByte(hadColor), 0, 1);
+            if(hadColor)
+            {
+                var colors = ToByte(mesh.colors);
+                fs.Write(colors, 0, colors.Length);
+            }
+
+            fs.Write(uv, 0, uv.Length);
+
+            //判断UV2的存在并写入数据
+            //如果UV2存在则直接写入
+            bool hadUV2 = mesh.uv2.Length == mesh.vertexCount;
+            fs.Write(ToByte(hadUV2), 0, 1);
+            if(hadUV2)
+            {
+                var uv2 = ToByte(mesh.uv2);
+                fs.Write(uv2, 0, uv2.Length);
+            }
+
+            var trisCount = ToByte(mesh.triangles.Length);
+            var tris = ToByte(mesh.triangles);
+
+            fs.Write(trisCount, 0, trisCount.Length);
+            fs.Write(tris, 0, tris.Length);
+            */
+        }
+        Debug.Log(mesh.vertexCount);
+        Debug.Log(mesh.triangles.Length);
+
+        return;
+        //for(int i = 0; i < imgs.Length; i++)
+        //{
+        //    imgs[i] = i;
+        //}
+
+        //byte[] bytes = ObjectToBytes(imgs);
+
+        float[] fls = new float[32];
+        for(int i = 0; i < 32; i++)
+        {
+            fls[i] = i;
+        }
+
+        byte[] fbs = new byte[sizeof(float) * 32];
+        unsafe
+        {
+            fixed (float* ptrf = fls)
+            {
+                byte* bytes = (byte*)ptrf;
+                for (int i = 0; i < 32 * sizeof(float); ++i)
+                {
+                    fbs[i] = *bytes++;
+                }
+            }
+        }
+
+        using (FileStream fs = new FileStream("Assets/Save.dat", FileMode.OpenOrCreate))
+        {
+            fs.Write(fbs, 0, fbs.Length);
+        }
+
+
+        //imgs[0] = 1.0f;
+        //imgs[1] = 3.0f;
+
+        //unsafe
+        //{
+        //    fixed(float* ptr = &imgs[0])
+        //    {
+        //        byte[] bytes = null;
+
+        //    }
+        //}
+    }
+
+}
